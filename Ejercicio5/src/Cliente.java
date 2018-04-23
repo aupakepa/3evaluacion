8bce854578c737dbca0872baa5f131cf2c2c6f50
aupakepa <jmonforte@fontecabras.es>
2018-04-23 21:53:21 +0200

    5

---
+++ Ejercicio5/src/Cliente.java
@@ -0,0 +1,62 @@
+class Cliente {
+
+	private String nombre;
+	private Integer tipo;
+	private Integer cola;
+	static Integer numero = 1;
+
+	public Integer getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(Integer tipo) {
+		this.tipo = tipo;
+	}
+
+	public Integer getCola() {
+		return cola;
+	}
+
+	public void setCola(Integer cola) {
+		this.cola = cola;
+	}
+
+	public Cliente(Integer tipo, Integer cola) {
+		super();
+		this.tipo = tipo;
+		this.cola = cola;
+		this.nombre = "cliente"+numero;
+		numero++;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public static Integer getNumero() {
+		return numero;
+	}
+
+	public static void setNumero(Integer numero) {
+		Cliente.numero = numero;
+	}
+
+	public void atencion() {
+		this.tipo=this.tipo - 1;
+	}
+	public boolean terminado(){
+		if(this.tipo==0){
+			return true;
+		}else{
+			return false;
+		}
+		
+	}
+	@Override
+	public String toString() {
+		return "Cliente [nombre=" + nombre + ", tipo=" + tipo + ", cola=" + cola + "]";
+	}
+}
